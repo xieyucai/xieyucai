34d499e4284a85782e1e2ec4f55d615283a433f3
xiexie <729388757@qq.com>
2020-12-30 15:34:27 +0800

    校验

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,4 +10,15 @@ $(function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
+
+  var form = layui.form
+  form.verify({
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    repwd: function (vaule) {
+      var pwd = $('.reg-box[name=password]').val()
+      if (pwd != vaule) {
+        return '两次密码不一致'
+      }
+    }
+  })
 })
