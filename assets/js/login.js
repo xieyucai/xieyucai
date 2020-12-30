393754dc24539dda7161727a3380bc1a996e8b3f
xiexie <729388757@qq.com>
2020-12-30 17:55:04 +0800

    登录注册监听成功

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,15 +10,47 @@ $(function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
-
+  //表单校验
   var form = layui.form
   form.verify({
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
     repwd: function (vaule) {
       var pwd = $('.reg-box[name=password]').val()
-      if (pwd != vaule) {
+      if (pwd !== vaule) {
         return '两次密码不一致'
       }
     }
+  })
+
+  var layer = layui.layer
+  //注册功能，监听提交事件
+  $('#form_reg').on('submit', function (e) {
+    e.preventDefault()
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+      if (res.status != 0) {
+        return layer.msg('注册失败')
+      } else {
+        layer.msg('注册成功，请登录！')
+      }
+    })
+  })
+
+  //登录功能，监听提交事件
+  $('#form_login').on('submit', function (e) {
+    e.preventDefault()
+
+    $.post('http://ajax.frontend.itheima.net/api/login', $(this).serialize(), function (res) {
+      if (res.status != 0) {
+        return layer.msg('登录失败')
+      } else {
+        layer.msg('登录成功！')
+        localStorage.setItem('token', res.token)
+        location.href = '/index.html'
+      }
+    })
   })
 })
