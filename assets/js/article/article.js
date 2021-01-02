28b83294e0e5bd04297eec67f1bbeee1b04a7a70
changjinpeng <24303312@qq.com>
2021-01-02 11:57:46 +0800

    文章类别分类添加功能

--- assets/js/article/article.js
+++ assets/js/article/article.js
@@ -1,6 +1,33 @@
 $(function () {
   var layer = layui.layer
+  var indexAdd = null
   getarticle()
+  $('#btnAddCate').on('click', function () {
+    indexAdd = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '添加文章分类',
+      content: $('#dialog-add').html()
+    })
+  })
+
+  indexAdd = $('body').on('submit', '#form-add', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/my/article/addcates',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status != 0) {
+          return layer.msg('新增分类失败')
+        } else {
+          getarticle()
+          layer.msg('新增分类成功')
+          layer.close(indexAdd)
+        }
+      }
+    })
+  })
 })
 
 function getarticle() {
